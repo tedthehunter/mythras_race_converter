82e058dca5d46d6cfa11d4eb4d9e976953bdaaee
RJ <weinheimer.raymond@gmail.com>
2022-08-22 12:32:14 -0700

    defined primary classes

--- main.py
+++ main.py
@@ -9,9 +9,13 @@ class PolyDie:
         return f'{self.name} object'
     
 class Characteristic:
-    def __init__(self, name, *dice):
+    def __init__(self, name, bonus, *dice):
         self.name = name
+        self.bonus = bonus
         self.dice = dice
+        self.min_score = len(self.dice) + self.bonus
+        self.max_score = (len(self.dice) * self.dice[0].sides) + self.bonus
+        self.range = f'{self.min_score}-{self.max_score}'
     
     def __repr__(self):
         return f'{self.name} characteristic object'
@@ -29,6 +33,8 @@ d10 = PolyDie(10)
 d12 = PolyDie(12)
 d20 = PolyDie(20)
 
-strength = Characteristic('Strength', d2, d2, d2, d2)
+strength = Characteristic('Strength', 6, d6, d6)
 print(strength)
 print(strength.dice)
+print(strength.range)
+print(strength.bonus)
