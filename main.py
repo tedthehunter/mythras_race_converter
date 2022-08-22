0f9dae4f74aede45ae89ace1e2052e89791bdc03
RJ <weinheimer.raymond@gmail.com>
2022-08-22 14:53:10 -0700

    fixed bug that was preventing correct update of mins maxes and average roll results

--- main.py
+++ main.py
@@ -4,18 +4,18 @@ class PolyDie:
     def __init__(self, sides):
         self.sides = sides
         self.name = f'd{self.sides}'
+        self.avg = (1+self.sides)/2
         
     def __repr__(self):
         return f'{self.name} object'
+    
     
 class Characteristic:
     def __init__(self, name, die_sides, num_of_dice, bonus=0):
         self.name = name
         self.bonus = bonus
         self.initialize_dice(die_sides, num_of_dice)
-        self.min_score = num_of_dice + self.bonus
-        self.max_score = (len(self.dice) * self.dice[0].sides) + self.bonus
-        self.range = f'{self.min_score}-{self.max_score}'
+        
     
     def __repr__(self):
         return f'{self.name} characteristic object'
@@ -24,10 +24,15 @@ class Characteristic:
         self.dice = []
         for i in range(num_of_dice):
             self.dice.append(PolyDie(die_sides))
+        self.min_score = num_of_dice + self.bonus
+        self.max_score = (len(self.dice) * self.dice[0].sides) + self.bonus
+        self.range = f'{self.min_score}-{self.max_score}'
+        self.avg_score = self.dice[0].avg * num_of_dice
     
     def increment_num_of_dice(self):
         die_sides = self.dice[0].sides
-        self.dice.append(PolyDie(die_sides))
+        num_of_dice = len(self.dice) + 1
+        self.initialize_dice(die_sides, num_of_dice)
         
     def decrement_num_of_dice(self):
         if len(self.dice) > 1:
@@ -62,7 +67,7 @@ class Characteristic:
 class RacialProfile:
     def __init__(self, name='Human'):
         self.name = name
-        self.strength = Characteristic('Strength', 20, 3)
+        self.strength = Characteristic('Strength', 6, 3)
         self.dexterity = Characteristic('Dexterity', 6, 3)
         self.constitution = Characteristic('Constitution', 6, 3)
         self.size = Characteristic('Size', 6, 2, 6)
@@ -77,3 +82,16 @@ class RacialProfile:
 
 test = RacialProfile()
 print(test.strength.dice)
+print(test.strength.avg_score)
+test.strength.increment_num_of_dice()
+print(test.strength.dice)
+print(test.strength.avg_score)
+test.strength.increment_num_of_dice()
+print(test.strength.dice)
+print(test.strength.avg_score)
+test.strength.increment_num_of_dice()
+print(test.strength.dice)
+print(test.strength.avg_score)
+test.strength.increment_num_of_dice()
+print(test.strength.dice)
+print(test.strength.avg_score)
