b8700b1d57ad6ec4f36299cc877178931e116a49
RJ <weinheimer.raymond@gmail.com>
2022-08-22 14:31:50 -0700

    added methods to characteristic class that allow for incrementing and decrementing number of dice and size of dice within a characteristic

--- main.py
+++ main.py
@@ -12,9 +12,7 @@ class Characteristic:
     def __init__(self, name, die_sides, num_of_dice, bonus=0):
         self.name = name
         self.bonus = bonus
-        self.dice = []
-        for i in range(num_of_dice):
-            self.dice.append(PolyDie(die_sides))
+        self.initialize_dice(die_sides, num_of_dice)
         self.min_score = num_of_dice + self.bonus
         self.max_score = (len(self.dice) * self.dice[0].sides) + self.bonus
         self.range = f'{self.min_score}-{self.max_score}'
@@ -22,10 +20,49 @@ class Characteristic:
     def __repr__(self):
         return f'{self.name} characteristic object'
     
+    def initialize_dice(self, die_sides, num_of_dice):
+        self.dice = []
+        for i in range(num_of_dice):
+            self.dice.append(PolyDie(die_sides))
+    
+    def increment_num_of_dice(self):
+        die_sides = self.dice[0].sides
+        self.dice.append(PolyDie(die_sides))
+        
+    def decrement_num_of_dice(self):
+        if len(self.dice) > 1:
+            self.dice.pop()
+        
+    def increment_dice_size(self):
+        die_sides = self.dice[0].sides
+        num_of_dice = len(self.dice)
+        if die_sides < 12:
+            die_sides += 2
+            self.initialize_dice(die_sides, num_of_dice)
+        elif die_sides == 12:
+            die_sides = 20
+            self.initialize_dice(die_sides, num_of_dice)
+        else:
+            return f'Dice size at maximum!'
+        
+    def decrement_dice_size(self):
+        die_sides = self.dice[0].sides
+        num_of_dice = len(self.dice)
+        if die_sides == 20:
+            die_sides = 12
+            self.initialize_dice(die_sides, num_of_dice)
+        elif die_sides > 2:
+            die_sides -= 2
+            self.initialize_dice(die_sides, num_of_dice)
+        else:
+            return f'Dice size at minimum!'
+        
+    
+    
 class RacialProfile:
     def __init__(self, name='Human'):
         self.name = name
-        self.strength = Characteristic('Strength', 6, 3)
+        self.strength = Characteristic('Strength', 20, 3)
         self.dexterity = Characteristic('Dexterity', 6, 3)
         self.constitution = Characteristic('Constitution', 6, 3)
         self.size = Characteristic('Size', 6, 2, 6)
@@ -34,10 +71,9 @@ class RacialProfile:
         self.charisma = Characteristic('Charisma', 6, 3)
         self.power = Characteristic('Power', 6, 3)
         
+    def __repr__(self):
+        return f'{self.name} profile object'
+        
 
-
-
-intelligence = Characteristic('Intelligence', 6, 2, 6)
-print(intelligence)
-print(intelligence.range)
-print(intelligence.dice)
+test = RacialProfile()
+print(test.strength.dice)
