c7beabeda5270d4be65e46b2929454abecb4ae93
RJ <weinheimer.raymond@gmail.com>
2022-08-23 11:42:10 -0700

    FINALLY got the distribution function working going to clean up programs, try to consolidate all into main and delete scratch and stats now working on numpy visualizations

--- scratch.py
+++ scratch.py
@@ -1,10 +1,10 @@
 import itertools
 
-def possible_combinations(*lists):
+def possible_combinations(list_of_lists):
     results = []
-    for i in itertools.product(*lists):
+    for i in itertools.product(*list_of_lists):
         results.append(sum(i))
-    print(results)
+    return results
         
 test1 = [i for i in range(6)]
 test2 = [i for i in range(6)]
@@ -12,4 +12,4 @@ test3 = [i for i in range(6)]
 
 test4 = [test1, test2, test3]
   
-possible_combinations(test4)
+# possible_combinations(test4)
--- stats.py
+++ stats.py
@@ -1,30 +1,10 @@
 import random, itertools
-from main import PolyDie
+from main import PolyDie, Characteristic, RacialProfile, test
 from scratch import possible_combinations
 
-# def roll_sample():
-#     num_roll = 6000000
 
-#     frequency = [0 for i in range(6)]
-
-#     for roll in range(num_roll):
-#         face = random.randrange(1, 7)
-#         frequency[face - 1] += 1
-        
-#     print(f'Face{"Frequency":>13}')
-#     for i, j in enumerate(frequency):
-#         print(f'{i + 1:>4}{j:>13}')
     
-
-def total_poss_outcomes(*dice):
-    lists = [die.get_range() for die in dice]
-    results = possible_combinations()
-    
-  
-total_poss_outcomes(PolyDie(6), PolyDie(6), PolyDie(6))    
-                           
-                           
-# x = [i for i in range(6)]
-# y = [i for i in range(6)]
-# z = [i for i in range(6)]
-# print([i+j+k for i in x for j in y for k in z])
+def total_poss_outcomes():
+    # makes a list with 0 for each possible outcome
+    outcomes = [0 for i in range(len(dice), (len(dice) * dice[0].sides) + 1)]
+    # then add each possible result of dice and increment its place in outcomes
